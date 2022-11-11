20b37576290fef17aa103953e85647709e51356a
rawand1998 <rawandgaradh@gmail.com>
2022-11-11 11:22:10 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './Components/Header';
 import NavBar from './Components/Navbar';
 import './styling/index'
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
      <NavBar/>
+     <Header/>
     </div>
   );
 }
